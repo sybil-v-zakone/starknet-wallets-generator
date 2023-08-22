104d26a27bf2d267b334009ed134371408bd2902
Roman Elberg <roma.elberg@gmail.com>
2023-08-22 14:04:42 +0300

    Constants refactoring

--- sdk/cex.py
+++ sdk/cex.py
@@ -2,7 +2,7 @@ from ccxt import okx
 from loguru import logger
 
 from config import OKX_API_CONFIG, CEX_WITHDRAW_FEE, LOAD_OKX_API_CONFIG_FROM_ENV
-from constants import okx_constants
+from constants import OKX_WITHDRAW_OPTIONS
 from sdk.load_from_env import load
 
 
@@ -19,20 +19,20 @@ def okx_withdraw(
     exchange_config = get_config()
     exchange = okx(exchange_config)
     try:
-        chain_name = f"{okx_constants['symbol_withdraw']}-{okx_constants['network']}"
+        chain_name = f"{OKX_WITHDRAW_OPTIONS['symbol_withdraw']}-{OKX_WITHDRAW_OPTIONS['network']}"
         logger.info(f"Withdraw {amount_to_withdrawal} {chain_name} from OKX to {to_address}")
         exchange.withdraw(
-            okx_constants['symbol_withdraw'],
+            OKX_WITHDRAW_OPTIONS['symbol_withdraw'],
             amount_to_withdrawal,
             to_address,
             params={
                 "toAddress": to_address,
                 "chain": chain_name,
-                "dest": okx_constants["dest"]["ON_CHAIN"],
+                "dest": OKX_WITHDRAW_OPTIONS["dest"]["ON_CHAIN"],
                 "fee": CEX_WITHDRAW_FEE,
                 "pwd": '-',
                 "amt": amount_to_withdrawal,
-                "network": okx_constants['network']
+                "network": OKX_WITHDRAW_OPTIONS['network']
             })
 
         logger.success(f"[OKX] withdraw {amount_to_withdrawal} {chain_name}")
